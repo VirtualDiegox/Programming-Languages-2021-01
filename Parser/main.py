59842545a8b1bc056601f51cbd5560985c2a44de
VirtualDiegox <dbayonac@unal.edu.co>
2021-04-29 23:29:14 -0500

    necesita arreglos, first and follow ok)?

--- Parser/main.py
+++ Parser/main.py
@@ -1,10 +1,10 @@
 import Psilex
-
+from Psilex import Token
 Tokens = Psilex.Tokens
 #for tok in Tokens:
     #print("<{},{},{}>".format(tok.type, tok.line, tok.column))
 
-token = Tokens.pop(0)
+
 
 
 
@@ -29,6 +29,7 @@ def CALCULAR_PRIMEROS(no_terminal):
     REGLAS = [rule for rule in grammar if rule[0]==no_terminal]
     for R in REGLAS:
         REGLA = R[1]
+        
         conjunto_final = conjunto_final + PRIMEROS(REGLA)
     return conjunto_final
         
@@ -61,45 +62,46 @@ def PRIMEROS(REGLA):
                         p = CALCULAR_PRIMEROS(cadena[0])
                     primeros = primeros + p
             return primeros
+
+ 
+
+
+def SIGUIENTES(no_terminal):
+    follow_ = []
+
+    RULES = grammar
+    if no_terminal == start:
+        follow_.append('$')
+    for nt,rule in RULES:
+        if no_terminal in rule:
+            n = len(follow_)
+            following_str = rule[rule.index(no_terminal)+1:]
+            if following_str == []:
+                if nt == no_terminal:
+                    continue
+                else:
+                    follow_ = follow_ + SIGUIENTES(nt)
+            else:
+                follow_2 = PRIMEROS(following_str)
+                if "EPSILON" in follow_2:
+                    follow_2.remove("EPSILON")
+                    follow_ = follow_ + follow_2
+                    follow_ = follow_ + SIGUIENTES(nt)
+                else:
+                    follow_ = follow_ + follow_2
+
+            follow_ = list(dict.fromkeys(follow_))
+            if n != len(follow_):
+                return follow_
+    return follow_
+    
         
 
-def CALCULAR_SIGUIENTES(no_terminal):
-    conjunto_final = []
-    if no_terminal == start:
-        conjunto_final.append("$")
-    
-    REGLAS = []
-    for rule in grammar:
-        if no_terminal in rule[1]:
-            REGLAS.append(rule)
-    #REGLAS = [rule for rule in grammar if (no_terminal in rule[1])]
-    
-    for R in REGLAS:
-        REGLA = R
-        conjunto_final = conjunto_final + SIGUIENTES(REGLA,no_terminal)
-    return conjunto_final
+        
+        
 
 
-def SIGUIENTES(R,no_terminal):
-    siguientes = []
-    nt_regla = R[0]
-    REGLA = R[1]
-    
-    index_nt = REGLA.index(no_terminal)
-    cadena_b = REGLA[index_nt+1:]
-    
-    if cadena_b == []:
-        cadena_b.append("EPSILON")
-    
-    if "EPSILON" in PRIMEROS(cadena_b):
-        primeros_b = PRIMEROS(cadena_b)
-        primeros_b.remove("EPSILON")
-        siguientes = siguientes + primeros_b
-        if "EPSILON" in PRIMEROS(cadena_b):
-            siguientes = siguientes + CALCULAR_SIGUIENTES(nt_regla)
-    else:
-        siguientes = siguientes + PRIMEROS(cadena_b)
-    return siguientes
+
 
 
 def PREDICT(A):
@@ -108,9 +110,10 @@ def PREDICT(A):
     C_PREDICT = []
     
     if "EPSILON" in PRIMEROS(REGLA):
+        
         C = PRIMEROS(REGLA)
         C.remove("EPSILON")
-        C_PREDICT = C + CALCULAR_SIGUIENTES(nt_regla)
+        C_PREDICT = C + SIGUIENTES(nt_regla)
         return C_PREDICT
     else:
         C = PRIMEROS(REGLA)
@@ -118,36 +121,47 @@ def PREDICT(A):
         return C_PREDICT
 
 
-#for x in range(0, len(grammar)):
-    #print(PREDICT(grammar[x]))
-
 def EMPAREJAR(token_esperado):
-    if token == token_esperado:
+    global token
+    if token.type == token_esperado:
         token = Tokens.pop(0)
     else:
         errorSintactico([token_esperado])
 
-def errorSintactico(token):
+def errorSintactico(esperados):
+    print(token.value)
+    print(token.type)
+    print(esperados)
     print("Error sintactico")
+    exit()
 
 def State(n_t):
-    REGLAS = [rule for rule in grammar if rule[0]==n_t]
+    
+    
     PREDICCIONES = []
-    for rule in REGLAS:
-        PREDICCIONES.append(PREDICT(rule))
+    REGLAS = []
+    for nt,rule in grammar:
+        
+        if nt == n_t:
+            REGLAS.append([nt,rule])
+            PREDICCIONES.append(PREDICT([nt,rule]))
     for p in range(0,len(PREDICCIONES)):
-        if token in PREDICCIONES[p]:
+        if token.type in PREDICCIONES[p]:
             RULE = REGLAS[p][1]
             for part in RULE:
                 if part in no_Terminals:
                     State(part)
                 if part in terminals:
+                    print("empareje")
+                    print(part)
                     EMPAREJAR(part)
                 if part == "EPSILON":
                     pass
-        return
-    
-    errorSintactico(PREDICCIONES[p])
+            return
+    terminals_regla = []
+    for p in PREDICCIONES:
+        terminals_regla = terminals_regla + p
+    errorSintactico(terminals_regla)
 
 def print_predictions():
 
@@ -160,7 +174,76 @@ def print_predictions():
     predictionsSets = []
     for c in grammar:
         predictionsSets.append(PREDICT(c))
+    for p in predictionsSets:
+        print(p)
 
-    print(predictionsSets)
+################################################################Parsing################################
 
-print_predictions()
+#get grammar
+with open("Psigrammar.txt", "r") as f:
+    new_grammar = []
+    rules = f.readlines()
+    for rule in rules:
+        rule_type = []
+        rule = rule.rstrip()
+        part_of_rule = rule.split(" ")
+        nt = part_of_rule[0]
+        rule_list = part_of_rule[2:]
+        rule_type.append(nt)
+        rule_type.append(rule_list)
+        new_grammar.append(rule_type)
+    clean_grammar = []
+    for rule in new_grammar:
+        if rule[0] != "":
+            clean_grammar.append(rule)
+    grammar = clean_grammar
+
+
+
+#get not_terminals
+no_Terminals = []
+for rule in clean_grammar:  
+    nt = rule[0]
+    if nt not in no_Terminals:
+        no_Terminals.append(nt)
+
+#get terminals
+from Psilex import tokens
+terminals = tokens
+
+#Tokens = []
+#Tokens.append(Token("ant",None,0,0))
+#Tokens.append(Token("cat",None,0,0))
+#Tokens.append(Token("all",None,0,0))
+
+Tokens.append(Token("EOF", None, 0, 0))
+token = Tokens.pop(0)
+
+primeros_set = {i: set() for i in no_Terminals}
+siguientes_set = {i: set() for i in no_Terminals}
+
+
+
+#get start
+start = no_Terminals[0]
+
+#get first tokens
+
+
+
+#begin
+State(start)
+
+
+
+
+
+
+
+    
+
+if token.type != "EOF":
+    print(token)
+    errorSintactico(["EOF"])
+else:
+    print("El analisis sintactico ha finalizado exitosamente.")
